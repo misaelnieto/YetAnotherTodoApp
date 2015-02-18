721adfb58e20a24578380ab9d659c79e582a343f
tzicatl <nnieto@noenieto.com>
2015-02-17 23:22:52 -0800

    Comenzamos con el trabajo de angularjs

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,12 +1,12 @@
 <!-- main content starts here -->
-<div class="rows" ng-app="YetAnotherTodoApp">
+<div class="rows" ng-controller="TaskLists">
     <div class="large-12 columns">
         <dl class="accordion" data-accordion>
-          <dd class="accordion-navigation">
-            <a href="#tasklist-0">
-                Tareas de casa
+          <dd class="accordion-navigation" ng-repeat="tsk_lst in task_lists">
+            <a href="#tasklist-{{tsk_lst.id}}">
+                {{ tsk_lst.title }}
             </a>
-            <div id="tasklist-0" class="content active">
+            <div id="tasklist-{{tsk_lst.id}}" class="content">
                 <table>
                   <thead>
                     <tr>
@@ -17,48 +17,13 @@
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
+                    <tr ng-repeat="tsk in tsk_lst.tasks">
                       <td>
-                        Content Goes Here
+                        {{tsk.text}}
                       </td>
-                      <td><a href="#" class="button tiny" title="Marcar la tarea como terminada"><i class="foundicon-checkmark"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>
-                        This is longer content Donec id elit non mi porta gravida at eget metus.
-                        </td>
-                      <td><a href="#" class="button tiny" title="Marcar la tarea como terminada"><i class="foundicon-checkmark"></i></a></td>
-                    </tr>
-                  </tbody>
-                </table>
-            </div>
-          </dd>
-          <dd class="accordion-navigation">
-            <a href="#tasklist-1">
-                Pendientes del auto
-            </a>
-            <div id="tasklist-1" class="content active">
-                <table>
-                  <thead>
-                    <tr>
-                      <th width="800">
-                        Tarea
-                      </th>
-                      <th class="mark-button">Marcar como terminada</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
                       <td>
-                        Content Goes Here
+                        <a href="#" class="button tiny" title="Marcar la tarea como terminada"><i class="foundicon-checkmark"></i></a>
                       </td>
-                      <td><a href="#" class="button tiny" title="Marcar la tarea como terminada"><i class="foundicon-checkmark"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>
-                        This is longer content Donec id elit non mi porta gravida at eget metus.
-                        </td>
-                      <td><a href="#" class="button tiny" title="Marcar la tarea como terminada"><i class="foundicon-checkmark"></i></a></td>
                     </tr>
                   </tbody>
                 </table>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,61 @@
 var YetAnotherTodoApp = angular.module('YetAnotherTodoApp', []);
 
-YetAnotherTodoApp.controller('PhoneListCtrl', function ($scope) {
+YetAnotherTodoApp.controller('TaskLists', function ($scope) {
   $scope.task_lists = [
     {
+
+        'id':0,
         'title': 'Default',
         'tasks': [
             {
                 id: 1,
-                text: 'Lorem ipsum',
+                text: 'Lorem ipsum 1',
                 created: '',
                 updated: ''
             },
             {
                 id: 2,
-                text: 'Lorem ipsum',
+                text: 'Lorem ipsum 2',
                 created: '',
                 updated: ''
             },
         ]
     },
+    {
+        'id':1,
+        'title': 'Tareas de casa',
+        'tasks': [
+            {
+                id: 1,
+                text: ' This is longer content Donec id elit non mi porta gravida at eget metus. ',
+                created: '',
+                updated: ''
+            },
+            {
+                id: 2,
+                text: 'Panda german iron man top bear star wars no bad ',
+                created: '',
+                updated: ''
+            },
+        ]
+    },
+    {
+        'id':2,
+        'title': 'Tareas del trabajo',
+        'tasks': [
+            {
+                id: 1,
+                text: 'Crear API REST',
+                created: '',
+                updated: ''
+            },
+            {
+                id: 2,
+                text: 'Instalar servidor de bases de datos.',
+                created: '',
+                updated: ''
+            },
+        ]
+    }
   ];
 });
